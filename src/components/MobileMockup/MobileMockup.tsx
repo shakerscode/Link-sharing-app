8458ea4c674950be88197ec3fb4578aea4451daf
Shaker Ahamed <ahamedshaker7@gmail..com>
2024-10-11 11:12:45 +0600

    fix: fixed few issues

--- src/components/MobileMockup/MobileMockup.tsx
+++ src/components/MobileMockup/MobileMockup.tsx
@@ -17,7 +17,7 @@ function MobileMockup({
   const [allLinkLists, setAllLinkList] = useState<IUserPlatformList[] | null>(
     null
   );
-  const arr = [1, 2, 3, 4, 5];
+  const arr = [1, 2, 3, 4];
 
   const links = JSON.parse(localStorage.getItem("savedLinkLists"));
   const info = JSON.parse(localStorage.getItem("uInfo"));
@@ -33,9 +33,9 @@ function MobileMockup({
 
   return (
     <>
-      {showMockup && <img src={mobileMoc} alt="" className="w-full h-full"/>}
+      {showMockup && <img src={mobileMoc} alt="" className="w-full h-full" />}
       <div
-        className="w-2/3 h-full absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center"
+        className="w-2/3 h-full absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center overflow-y-scroll "
         style={{
           left: "50%",
           top: "0%",
@@ -57,46 +57,52 @@ function MobileMockup({
         ) : (
           <div className="w-1/3 h-4 mt-4 rounded-full bg-gray-200"></div>
         )}
-        <div className="mt-12"></div>
-        {allLinkLists?.length > 0
-          ? allLinkLists?.map((list, i) => (
-              <Link to={list?.platform_url} target="_blank" className="w-full">
+        <div className={"mt-5"}></div>
+        <div className="w-full overflow-y-scroll h-[250px] hide-scrollbar">
+          {allLinkLists?.length > 0
+            ? allLinkLists?.map((list, i) => (
+                <Link
+                  to={list?.platform_url}
+                  target="_blank"
+                  className="w-full"
+                >
+                  <div
+                    key={i}
+                    className={`w-full flex items-center transition-all duration-300 px-5 cursor-pointer mt-4 h-12 rounded-xl ${
+                      list?.platform_name.toLowerCase() === "github"
+                        ? "bg-gray-900 hover:bg-gray-700"
+                        : list?.platform_name.toLowerCase() === "linkedin"
+                        ? "bg-blue-500 hover:bg-blue-400"
+                        : list?.platform_name.toLowerCase() === "youtube"
+                        ? "bg-red-600 hover:bg-red-500"
+                        : list?.platform_name.toLowerCase() === "twitter"
+                        ? "bg-sky-500 hover:bg-sky-400"
+                        : ""
+                    }`}
+                  >
+                    {platforms?.find(
+                      (platform) => platform?.name === list?.platform_name
+                    )?.icon && (
+                      <span className="mr-2 text-white">
+                        {
+                          platforms?.find(
+                            (platform) => platform?.name === list?.platform_name
+                          )?.icon
+                        }
+                      </span>
+                    )}
+                    <p className="text-white text-sm">{list?.platform_name}</p>
+                    <IoMdArrowForward className="flex-end flex ml-auto text-white" />
+                  </div>
+                </Link>
+              ))
+            : arr.map((_, i: number) => (
                 <div
                   key={i}
-                  className={`w-full flex items-center transition-all duration-300 px-5 cursor-pointer mt-4 h-12 rounded-xl bg-gray-200 ${
-                    list?.platform_name.toLowerCase() === "github"
-                      ? "bg-gray-900 hover:bg-gray-700"
-                      : list?.platform_name.toLowerCase() === "linkedin"
-                      ? "bg-blue-500 hover:bg-blue-400"
-                      : list?.platform_name.toLowerCase() === "youtube"
-                      ? "bg-red-600 hover:bg-red-500"
-                      : list?.platform_name.toLowerCase() === "twitter"
-                      ? "bg-sky-500 hover:bg-sky-400"
-                      : ""
-                  }`}
-                >
-                  {platforms?.find(
-                    (platform) => platform?.name === list?.platform_name
-                  )?.icon && (
-                    <span className="mr-2 text-white">
-                      {
-                        platforms?.find(
-                          (platform) => platform?.name === list?.platform_name
-                        )?.icon
-                      }
-                    </span>
-                  )}
-                  <p className="text-white text-sm">{list?.platform_name}</p>
-                  <IoMdArrowForward className="flex-end flex ml-auto text-white" />
-                </div>
-              </Link>
-            ))
-          : arr.map((_, i: number) => (
-              <div
-                key={i}
-                className="w-full mt-4 h-12 rounded-xl bg-gray-200"
-              ></div>
-            ))}
+                  className="w-full mt-4 h-12 rounded-xl bg-gray-200"
+                ></div>
+              ))}
+        </div>
       </div>
     </>
   );
