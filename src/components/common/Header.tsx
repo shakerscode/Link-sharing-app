9e9f27fa75b7bd27c560ed0e3edc63be76177d01
Shaker Ahamed <ahamedshaker7@gmail..com>
2024-10-13 13:51:28 +0600

    fix: made preview privet

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -89,7 +89,7 @@ export default function Header() {
 
       {/* action btn  */}
       <div className="flex gap-2">
-        <Link to={"/preview"}>
+        {isAuthenticated && <Link to={"/preview"}>
           <button
             className={
               "border border-violet-500 text-violet-500 px-5 hover:bg-violet-500 hover:text-white transition duration-300 py-2 rounded-lg text-sm font-semibold flex items-center justify-center gap-1.5"
@@ -99,7 +99,7 @@ export default function Header() {
 
             <p className="hide-name">Preview</p>
           </button>
-        </Link>
+        </Link>}
         {isAuthenticated ? (
           <button
             onClick={handleLogout}
